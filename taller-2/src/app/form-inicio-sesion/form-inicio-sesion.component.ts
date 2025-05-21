8ea998707e7c18ea6c4492d6abe338b861d7852c
Lucía Alarcón <lalarcon782@alumno.unlam.edu.ar>
2025-05-21 18:47:37 -0300

    form inicio sesión y registro

---
+++ taller-2/src/app/form-inicio-sesion/form-inicio-sesion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-inicio-sesion',
+  imports: [],
+  templateUrl: './form-inicio-sesion.component.html',
+  styleUrl: './form-inicio-sesion.component.css'
+})
+export class FormInicioSesionComponent {
+
+}
