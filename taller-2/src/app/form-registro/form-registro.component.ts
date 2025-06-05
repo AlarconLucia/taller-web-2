61219606589da358e9af7a8cdefdb5f9b96d77a7
Lira <ailinvara@gmail.com>
2025-06-05 20:57:20 -0300

    agrega componentes shared (header y footer), inicio y productos

--- taller-2/src/app/form-registro/form-registro.component.ts
+++ taller-2/src/app/form-registro/form-registro.component.ts
@@ -1,24 +1,41 @@
 import { Component, signal } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-form-registro',
   imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './form-registro.component.html',
-  styleUrl: './form-registro.component.css'
+  styleUrl: './form-registro.component.css',
 })
 export class FormRegistroComponent {
   registro = signal<FormGroup>(
-    new FormGroup(
-      {
-        name: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.minLength(3)] }),
-        lastName: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.minLength(3)] }),
-        address: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.minLength(5)] }),
-        email: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.email] }),
-        password: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.minLength(5)] }),
-
-      }
-    )
+    new FormGroup({
+      name: new FormControl('', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.minLength(3)],
+      }),
+      lastName: new FormControl('', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.minLength(3)],
+      }),
+      address: new FormControl('', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.minLength(5)],
+      }),
+      email: new FormControl('', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.email],
+      }),
+      password: new FormControl('', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.minLength(5)],
+      }),
+    })
   );
 }
