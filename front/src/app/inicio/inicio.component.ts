eb25c3845661cc54221b7b15d08036b0291ff9e0
Lira <ailinvara@gmail.com>
2025-07-05 19:05:44 -0300

    Agrega modelos 3D al banner del inicio

--- front/src/app/inicio/inicio.component.ts
+++ front/src/app/inicio/inicio.component.ts
@@ -1,11 +1,26 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {
+  Component,
+  Inject,
+  PLATFORM_ID,
+  CUSTOM_ELEMENTS_SCHEMA,
+} from '@angular/core';
+import { isPlatformBrowser, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css',
+  styleUrls: ['./inicio.component.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class InicioComponent {}
+export class InicioComponent {
+  public isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+    if (this.isBrowser) {
+      import('@google/model-viewer');
+    }
+  }
+}
