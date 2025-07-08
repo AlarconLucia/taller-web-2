bf536c7ae2d0c8544145ad44970d80a8e7c03a93
Lira <ailinvara@gmail.com>
2025-07-08 20:04:22 -0300

    Corrige subida de img

--- back/src/server/server.ts
+++ back/src/server/server.ts
@@ -3,34 +3,37 @@ import cors from 'cors';
 
 interface Options {
     port: number;
-    routes:Router
+    routes: Router;
 }
 
 export class Server {
 
-    private app = express()
+    private app = express();
+    private readonly port: number;
+    private readonly routes: Router;
 
-    private readonly port:number;
-    private readonly routes:Router;
-
-    constructor(options:Options){
-        const {port,routes} = options
-        this.port = port 
-        this.routes = routes
+    constructor(options: Options) {
+        const { port, routes } = options;
+        this.port = port;
+        this.routes = routes;
     }
 
     async start() {
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
 
-        this.app.use(express.json())
-        this.app.use(express.urlencoded({extended : true}))
+        const corsOptions = {
+            origin: 'http://localhost:4200',
+            optionsSuccessStatus: 200
+        };
+        this.app.use(cors(corsOptions));
 
-        this.app.use(cors())
+        this.app.use(express.static('public'));
 
         this.app.use(this.routes);
 
-        this.app.listen(this.port, ()=>{
+        this.app.listen(this.port, () => {
             console.log("Servidor corriendo en el puerto " + this.port);
-        })
-     }
+        });
+    }
 }
-
